b85fda223ef911db92645df189ea551b16d00ab6
Bill Yengo <yengobillarnold@gmail.com>
2025-03-07 06:41:23 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import '@styles/global.css';
+import { inter } from './ui/fonts/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
+}
