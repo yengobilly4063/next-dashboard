7fb7cf8893f92d90574ffdccb0390adf2906d2c3
Bill Yengo <yengobillarnold@gmail.com>
2025-03-07 07:28:19 +0100

    added preload

--- app/ui/fonts/fonts.ts
+++ app/ui/fonts/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'], preload: false });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'], preload: false });
